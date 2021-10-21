1090de7642e1682c520895a46679d97aa6211805
takuya828 <takuya11070221@gmail.com>
2021-10-21 12:44:59 +0000

    [Add]新規投稿画面、投稿一覧画面作成、投稿機能追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
   def active_for_authentication?
         super && (self.is_delete === "Available")
   end
+  
+  has_many :posts, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '6c5bc96bcd3b9941cfe80eb7109203225757d03fcc14262687fe6948ad42d89f8680bb36f4e6ddb9098efbb35940c649a4c736167b0a9f3eba30a9290309ff33'
