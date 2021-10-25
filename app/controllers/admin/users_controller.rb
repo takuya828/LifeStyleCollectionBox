d93ad9e6e69435ed73c84ca29c51a49d086d697e
takuya828 <takuya11070221@gmail.com>
2021-10-25 13:59:22 +0000

    [Add] 管理者のユーザー詳細ページ、ユーザー編集ページ作成

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,4 +2,33 @@ class Admin::UsersController < ApplicationController
   def index
     @users = User.all
   end
+  
+  def show
+    @user = User.find(params[:id])
+    @posts = User.find(params[:id]).posts
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+       flash[:success] = "You have updated info successfully."
+       redirect_to  admin_user_path(@user.id)
+    elsif @user.update(is_delete: true)
+       flash[:success] = "You have updated status successfully."
+      redirect_to admin_user_path(@user.id)
+    else
+       flash[:danger] = "error"
+       redirect_to admin_user_path(@user.id)
+    end
+  end
+  
+  private
+
+   def user_params
+    params.require(:user).permit(:handle_name, :is_active)
+   end
 end
