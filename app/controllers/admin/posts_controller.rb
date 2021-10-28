a426e600c5007a433e709dcecd5a638537d36256
takuya828 <takuya11070221@gmail.com>
2021-10-28 15:18:11 +0000

    [Fix] 管理者投稿編集画面で削除機能追加、カテゴリー表示修正

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -2,10 +2,21 @@ class Admin::PostsController < ApplicationController
   def index
     @posts = Post.all
   end
-  
+
   def show
     @post = Post.find(params[:id])
     @post_comment = PostComment.new
   end
-  
+
+  def edit
+   @post = Post.find(params[:id])
+  end
+
+
+  def destroy
+    post = Post.find(params[:id])
+    post.destroy
+    redirect_to admin_posts_path
+  end
+
 end
