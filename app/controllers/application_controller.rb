bdfdc0b6242f59a71878a8203a247742d3fe9739
takuya828 <takuya11070221@gmail.com>
2021-11-05 06:20:52 +0000

    [Fix]会員新規登録、会員ログイン画面　レイアウト調整

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+    def after_sign_in_path_for(resource)
+     case resource
+        when Admin
+      admin_posts_path          #pathは設定したい遷移先へのpathを指定してください
+        when User
+      root_path              #ここもpathはご自由に変更してください
+     end
+    end
+
 end
