efb1d388a00465e1886bceb325edbe4dca2233fc
takuya828 <takuya11070221@gmail.com>
2021-11-18 13:45:13 +0000

    [Fix]migrationファイルの修正、リセットと不要なファイルの削除

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,4 +1,6 @@
 class Admin < ApplicationRecord
+  validates :email, presence: true
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
